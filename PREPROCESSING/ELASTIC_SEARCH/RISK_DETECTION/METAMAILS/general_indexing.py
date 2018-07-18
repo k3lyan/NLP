7b1d2cb11e3eba8e13261fe2ee13040e49a02697
k3lyan <kelyan.morfouesse@gmail.com>
2018-07-18 11:04:20 +0200

    update metamail indexing

--- PREPROCESSING/ELASTIC_SEARCH/RISK_DETECTION/METAMAILS/general_indexing.py
+++ PREPROCESSING/ELASTIC_SEARCH/RISK_DETECTION/METAMAILS/general_indexing.py
@@ -4,7 +4,7 @@ import json
 from elasticsearch import Elasticsearch
 
 #sys.argv[1]: file containing the headers paths, one path per line
-#sys.argv[2]: file containing the mails paths, one path per line
+#sys.argv[2]: file containing the metametamails paths, one path per line
 
 es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 
@@ -19,19 +19,18 @@ def get_files(paths):
         paths_list.close()
     return files_list
 
-def content_builder(header_files, mail_files):
-    header = []
-    mail = []
+def content_builder(header_files, metamail_files):
+    # Initialize data structures
     meta = []
     core = []
-    
+    header = {}
+    metamail = {}
+
     # MAILS DATA
-    for id_mail, path_mail in enumerate(mail_files):
-        mail = {}
-        with open(path_mail, 'r') as mail_file:
-            mail[path_mail.split('/')[-1].replace(' ','')] = balise_cleaner(mail_file.read())
-            print('MAIL: {}'.format(path_mail.split('/')[-1].replace(' ','')))
-            mail_file.close()
+    for id_metamail, path_metamail in enumerate(metamail_files):
+        with open(path_metamail, 'r') as metamail_file:
+            metamail[path_metamail.split('/')[-1].replace(' ','')] = balise_cleaner(metamail_file.read())
+            metamail_file.close()
     
     # HEADERS DATA
     for file_id, path_header in enumerate(header_files):
@@ -41,41 +40,43 @@ def content_builder(header_files, mail_files):
         content = [' '.join(field.split(':')[1:]).replace('\u00e9', u'é') for field in data_header]
 
         if (len(content) == 7):
-            header.append({'id_mail': path_header.split('/')[-1].replace(' ',''), 'sender' : content[0], 'timestamp': content[1],\
-                    'receiver': content[4], 'distribution': content[5], 'title': content[6]})
+            header[path_header.split('/')[-1].replace(' ','')] =  {'sender' : content[0], 'timestamp': content[1],\
+                    'receiver': content[4], 'distribution': content[5], 'title': content[6]}
         elif (len(content) == 6):
-            header.append({'id_mail': path_header.split('/')[-1].replace(' ',''), 'sender' : content[1], 'timestamp': content[2],\
-                    'receiver': content[3], 'distribution': content[4], 'title': content[5]})
+            header[path_header.split('/')[-1].replace(' ','')] = {'sender' : content[1], 'timestamp': content[2],\
+                    'receiver': content[3], 'distribution': content[4], 'title': content[5]}
         elif (len(content) == 5):
-            header.append({'id_mail': path_header.split('/')[-1].replace(' ',''), 'sender' : content[0], 'timestamp': content[1],\
-                    'receiver': content[2], 'distribution': content[3], 'title': content[4]})
+            header[path_header.split('/')[-1].replace(' ','')] =  {'sender' : content[0], 'timestamp': content[1],\
+                    'receiver': content[2], 'distribution': content[3], 'title': content[4]}
         elif(len(content) == 4):
-            header.append({'id_mail': path_header.split('/')[-1].replace(' ',''), 'sender' : content[0], 'timestamp': content[1],\
-                    'receiver': content[2], 'distribution': 'unknown', 'title': content[3]})
+            header[path_header.split('/')[-1].replace(' ','')] =  {'sender' : content[0], 'timestamp': content[1],\
+                    'receiver': content[2], 'distribution': 'unknown', 'title': content[3]}
         elif(len(content) == 3):
-            header.append({'id_mail': path_header.split('/')[-1].replace(' ',''), 'sender' : 'unknown', 'timestamp': 'unknown',\
-                    'receiver': content[0], 'distribution': content[1], 'title': content[2]})
+            header[path_header.split('/')[-1].replace(' ','')] =  {'sender' : 'unknown', 'timestamp': 'unknown',\
+                    'receiver': content[0], 'distribution': content[1], 'title': content[2]}
         elif(len(content) == 2):
-            header.append({'id_mail': path_header.split('/')[-1].replace(' ',''), 'sender' : content[0], 'timestamp': content[1],\
-                    'receiver': 'unknown', 'distribution': 'unknown', 'title': 'unknown'})
+            header[path_header.split('/')[-1].replace(' ','')] = {'sender' : content[0], 'timestamp': content[1],\
+                    'receiver': 'unknown', 'distribution': 'unknown', 'title': 'unknown'}
         elif(len(content) == 1):
-            header.append({'id_mail': path_header.split('/')[-1].replace(' ',''), 'sender' : content[0], 'timestamp': 'unknown',\
-                    'receiver': 'unknown', 'distribution': 'unknown', 'title': 'unknown'})
+            header[path_header.split('/')[-1].replace(' ','')] = {'sender' : content[0], 'timestamp': 'unknown',\
+                    'receiver': 'unknown', 'distribution': 'unknown', 'title': 'unknown'}
         elif(len(content) == 0):
-            header.append({'id_mail': path_header.split('/')[-1].replace(' ',''), 'sender' : 'unknown', 'timestamp': 'unknown',\
-                    'receiver': 'unknown', 'distribution': 'unknown', 'title': 'unknown'})
+            header[path_header.split('/')[-1].replace(' ','')] = {'sender' : 'unknown', 'timestamp': 'unknown',\
+                    'receiver': 'unknown', 'distribution': 'unknown', 'title': 'unknown'}
         else:
             print(len(content))
             print(path_header)
             print('INFORMATION IS  MISSING')
         print('HEADER: {}'.format(path_header.split('/')[-1].replace(' ','')))
-    for file_id in range(len(header)):
+    
+    for index, metamail_id in enumerate(header.keys()):
         # META DATA
-        if header[file_id]['id_mail'] in mail:
-            meta.append(json.JSONEncoder().encode({'index': {'_index': 'hydro', '_type': 'all', '_id': file_id}}))
-            header[file_id]['content'] = mail[header[file_id]['id_mail']]
-            print(header[file_id]['id_mail'])
-            core.append(json.JSONEncoder().encode(header[file_id]))
+        meta.append(json.JSONEncoder().encode({'index': {'_index': 'hydro', '_type': 'metamail', '_id': index}}))
+        features = header[metamail_id]
+        features['id_metamail'] = metamail_id 
+        features['content'] = metamail[metamail_id]
+        core.append(json.JSONEncoder().encode(features))
+        print('{}\n'.format(features))
     return (meta, core)
 
 with open('global.json', 'w') as json_file:
@@ -85,6 +86,6 @@ with open('global.json', 'w') as json_file:
         json_file.write('\n')
         json_file.write(core[doc])
         json_file.write('\n')
-        print('{} \n'.format(doc))
+        #print('{} \n'.format(doc))
     json_file.close()
 
