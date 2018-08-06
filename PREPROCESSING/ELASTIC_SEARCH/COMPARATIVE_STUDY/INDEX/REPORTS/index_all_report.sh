c530fc31937c5bf80a2effd183b76771e01a5c1f
k3lyan <kelyan.morfouesse@gmail.com>
2018-08-06 17:21:12 +0200

    Upload Elasticsearch module

--- PREPROCESSING/ELASTIC_SEARCH/COMPARATIVE_STUDY/INDEX/REPORTS/index_all_report.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-for i in `seq 0 172`;
-do
-	python3 report_indexing.py report_paths $i $i
-done
--- PREPROCESSING/ELASTIC_SEARCH/RISK_DETECTION/metapaths.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-targeted_directory='./meta_mails/'
-for i in $(ls "$targeted_directory")
-do
-	echo "$(realpath "$targeted_directory""$i")" >> metamail_paths
-done
-
